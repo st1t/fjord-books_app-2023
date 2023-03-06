12512d9b206044e5af760c103f4d19e91ea7ef83
Shota Ito <shota.ito.jp@gmail.com>
2023-03-06 21:00:29 +0900

    アバターのアップロード可能な拡張子をjpg, png, gifに限定するように修正

--- Gemfile
+++ Gemfile
@@ -84,3 +84,5 @@ gem 'carrierwave'
 gem 'devise'
 gem 'devise-i18n'
 gem 'kaminari'
+
+gem 'active_storage_validations', '~> 1.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
   has_one_attached :avatar
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :avatar, attached: true, content_type: %w[image/jpg image/png image/gif]
 end
