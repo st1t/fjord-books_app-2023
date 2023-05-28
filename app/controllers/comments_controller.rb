47023222aeaf599b671ba897a77d05eeb9d43b73
Shota Ito <shota.ito.jp@gmail.com>
2023-05-28 12:14:38 +0900

    コメントの削除機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  # before_action :set_comment, only: %i[show edit update destroy]
+  before_action :set_comment, only: %i[destroy]
   # GET /comments
   # def index
   #   @comments = Comment.order(:id).page(params[:page])
   # end
-  #
+
   # # GET /comments/1
   # def show
   #   @comment = Comment.new
   # end
-  #
+
   # # GET /comments/new
   def new
     @comment = Comment.new
   end
-  #
+
   # # GET /comments/1/edit
   # def edit; end
 
@@ -39,13 +39,16 @@ class CommentsController < ApplicationController
   #     render :edit, status: :unprocessable_entity
   #   end
   # end
-  #
-  # # DELETE /comments/1
-  # def destroy
-  #   @comment.destroy
-  #
-  #   redirect_to comments_url, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
-  # end
+
+  # DELETE /comments/1
+  def destroy
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to comments_url, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      redirect_to comments_url, alert: t('controllers.comments.error_not_owner')
+    end
+  end
 
   private
 
@@ -56,6 +59,6 @@ class CommentsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:author, :body, :commentable_id, :commentable_type)
+    params.require(:comment).permit(:author, :body, :commentable_id, :commentable_type, :user_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_024028) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_28_002653) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_024028) do
     t.string "commentable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -82,4 +84,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_024028) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
 end
