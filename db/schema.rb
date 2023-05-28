abd01dfd464d8c0d8fe27fdc32328328941c0798
Shota Ito <shota.ito.jp@gmail.com>
2023-05-28 12:25:43 +0900

    ユーザー名が未入力だったらemailアドレスを表示するように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_28_002653) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_28_031904) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_28_002653) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "author"
     t.string "body"
     t.integer "commentable_id"
     t.string "commentable_type"
