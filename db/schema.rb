f5a7cb1b830f12b422fe1a0cbcd7ad42d0919331
Shota Ito <shota.ito.jp@gmail.com>
2023-06-04 15:24:18 +0900

    reportsテーブルのbodyカラムにnot null制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_052039) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_062104) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_052039) do
 
   create_table "reports", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
