ef27d6a12a963fa02f2e42514179cb5f6d42900e
Shota Ito <shota.ito.jp@gmail.com>
2023-06-04 23:19:34 +0900

    commentのbodyにnot null制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_095818) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_141801) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_095818) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "commentable_id"
     t.string "commentable_type", null: false
     t.datetime "created_at", null: false
