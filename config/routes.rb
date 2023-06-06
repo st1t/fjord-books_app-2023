4cbd0df101841db63c4d21991f8c3945a56bd398
Shota Ito <shota.ito.jp@gmail.com>
2023-06-06 23:57:26 +0900

    Modify to allow all actions of report

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
-  resources :reports, only: %i(index show new edit create update destroy)
+  resources :reports
   resources :comments, only: %i(show new create destroy)
 end
