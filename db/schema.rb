05e88e6f9bc763e2b2bc24f9faed3c03c3b0e7a0
Shota Ito <shota.ito.jp@gmail.com>
2023-06-09 08:01:32 +0900

    commentsテーブルでcommentableなカラムにはreferenceを利用するように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_141801) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_234855) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,12 +50,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_141801) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "commentable_id"
-    t.string "commentable_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
-    t.index ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
