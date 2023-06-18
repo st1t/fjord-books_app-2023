8dc176f8426652e4140ab0825dbf02aa53a02de1
Shota Ito <shota.ito.jp@gmail.com>
2023-06-18 18:52:11 +0900

    reportを削除したときに外部キー制約を持つレコードを削除するように修正

--- app/models/mentioned_report.rb
+++ app/models/mentioned_report.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class MentionedReport < ApplicationRecord
-  has_many :report_mentioning_reports, dependent: :destroy
-  has_many :reports, through: :report_mentioning_reports
+  has_many :report_mentioned_reports, dependent: :destroy
+  has_many :reports, through: :report_mentioned_reports
 end
